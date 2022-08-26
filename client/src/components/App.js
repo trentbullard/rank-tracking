58837ce829cf1cfa0f374a4cfb310c81a8f7b012
trent bullard <trent.bullard@gmail.com>
2022-08-26 13:24:51 -0500

    remove next; start header and footer; fix index.html; add router and switch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
+import { Header } from './components/header/header';
 
 const App = () => {
   return (
     <div className="ui container">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <h1>Hello World</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   )
 }
---
+++ client/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => [
+  <h1 className="ui center aligned header" style={{ paddingTop: "3em" }} key="site-header">
+    <Link href="/">MultiRank</Link>
+    <span className="ui red sub header">BETA</span>
+  </h1>
+];
+
+export default Header;
