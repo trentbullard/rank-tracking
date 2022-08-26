816855fe7e85cd5b7a19538f5c46e58b4d749668
trent bullard <trent.bullard@gmail.com>
2022-08-26 17:30:52 -0500

    add menu to user button

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Container from '@mui/material/Container';
-
 import Header from './header/header';
 import Home from '../pages/home/home';
 
--- client/src/components/header/userArea.js
+++ client/src/components/header/userArea.js
@@ -1,12 +1,15 @@
-import React from "react";
+import * as React from "react";
 import Tooltip from "@mui/material/Tooltip";
 import Fab from "@mui/material/Fab";
 import PersonIcon from "@mui/icons-material/Person";
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const style = {
   margin: 0,
-  top: 20,
-  right: 20,
+  top: "1em",
+  right: "1em",
   bottom: "auto",
   left: "auto",
   position: "fixed",
@@ -16,7 +19,7 @@ const style = {
 const UnauthenticatedUserArea = props => {
   return (
     <Tooltip title="Login" placement="bottom">
-      <Fab color="error" aria-label="login" style={style} onClick={props.onClick}>
+      <Fab color="error" style={style} aria-label="login" onClick={props.onClick}>
         <PersonIcon fontSize="large" />
       </Fab>
     </Tooltip>
@@ -24,7 +27,32 @@ const UnauthenticatedUserArea = props => {
 };
 
 const AuthenticatedUserArea = props => {
-  return null;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  return (
+    <>
+      <Fab color="error" style={style} aria-label="settings" onClick={handleClick}>
+        <AdminPanelSettingsIcon fontSize="large" />
+      </Fab>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
 };
 
 const UserArea = props => {
