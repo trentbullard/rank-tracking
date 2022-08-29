711546076e868ba48e5074daf63bee9fad4b2563
trent bullard <trent.bullard@gmail.com>
2022-08-29 14:55:29 -0500

    add route for new league form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from '@mui/material';
 
+import { AuthContext } from '../contexts/AuthContext';
 import Header from './header/Header';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
-import { AuthContext } from '../contexts/AuthContext';
+import NewLeague from '../pages/league/NewLeague';
 
 const App = () => {
   const { currentUser } = React.useContext(AuthContext);
@@ -23,11 +24,19 @@ const App = () => {
       <Header currentUser={currentUser} />
       <Container>
         <Routes>
+          
           <Route path="/" element={
             <RequireAuth>
               <Home currentUser={currentUser} />
             </RequireAuth>
           } />
+
+          <Route path="leagues/new" element={
+            <RequireAuth>
+              <NewLeague />
+            </RequireAuth>
+          } />
+          
           <Route path="login" element={
             <RequireNoAuth>
               <Login currentUser={currentUser} />
