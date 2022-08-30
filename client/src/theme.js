42c66d5ce7b1d146a72713dc798c153b9a723969
trent bullard <trent.bullard@gmail.com>
2022-08-30 12:50:51 -0500

    implement dark theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,18 +1,49 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import blue from '@mui/material/colors/blue';
-import green from '@mui/material/colors/green';
 
-let theme = createTheme({
+let lightTheme = createTheme({
   palette: {
+    type: 'light',
     primary: {
-      main: blue[600],
+      main: '#33334b',
     },
     secondary: {
-      main: green[500],
+      main: '#f50057',
+    },
+    background: {
+      default: '#fafafa',
+      paper: '#fff',
     },
   },
 });
 
-theme = responsiveFontSizes(theme);
+let darkTheme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#64b4f6',
+    },
+    secondary: {
+      main: '#f6a664',
+    },
+    error: {
+      main: '#fb5b49',
+      light: '#fb7b6d',
+      dark: '#af3f33',
+    },
+    background: {
+      default: '#303030',
+      paper: '#424242',
+    },
+    text: {
+      primary: '#fff',
+      secondary: 'rgba(255, 255, 255, 0.7)',
+      disabled: 'rgba(255, 255, 255, 0.5)',
+      hint: 'rgba(255, 255, 255, 0.5)',
+    },
+  },
+});
 
-export default theme;
+lightTheme = responsiveFontSizes(lightTheme);
+darkTheme = responsiveFontSizes(darkTheme);
+
+export default darkTheme;
