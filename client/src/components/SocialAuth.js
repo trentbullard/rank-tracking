4b6723d0353627b7ea89e22caab106a74cdc6159
trent bullard <trent.bullard@gmail.com>
2022-08-30 15:33:25 -0500

    fix social auth sizes

--- client/src/components/SocialAuth.js
+++ client/src/components/SocialAuth.js
@@ -5,37 +5,18 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 const SocialAuth = () => {
+  const iconHeight = '1.75em';
+  
   return (
     <Stack spacing={2} direction="row">
-      <IconButton
-        sx={{
-          border: "1px solid #ccc",
-          borderRadius: "5px",
-          padding: "0.5675rem",
-          flex: 1,
-        }}
-      >
-        <GoogleIcon sx={{ color: "#DF3E30" }} width={22} height={22} />
+      <IconButton variant="outlined" sx={{ flex: 1 }}>
+        <GoogleIcon sx={{ color: "#DF3E30", fontSize: iconHeight }} />
       </IconButton>
-      <IconButton
-        sx={{
-          border: "1px solid #ccc",
-          borderRadius: "5px",
-          padding: "0.5675rem",
-          flex: 1,
-        }}
-      >
-        <FacebookIcon sx={{ color: "#1877F2" }} width={22} height={22} />
+      <IconButton variant="outlined" sx={{ flex: 1 }}>
+        <FacebookIcon sx={{ color: "#1877F2", fontSize: iconHeight }} />
       </IconButton>
-      <IconButton
-        sx={{
-          border: "1px solid #ccc",
-          borderRadius: "5px",
-          padding: "0.5675rem",
-          flex: 1,
-        }}
-      >
-        <TwitterIcon sx={{ color: "#1C9CEA" }} width={22} height={22} />
+      <IconButton variant="outlined" sx={{ flex: 1 }} >
+        <TwitterIcon sx={{ color: "#1C9CEA", fontSize: iconHeight }} />
       </IconButton>
     </Stack>
   );
