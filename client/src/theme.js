7b0830030930237cd77f3bd6c4c1fdb2d7509e34
trent bullard <trent.bullard@gmail.com>
2022-08-30 18:27:20 -0500

    add fonts to theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -12,6 +12,48 @@ let theme = createTheme({
       xxl: 1440,
     },
   },
+  typography: {
+    fontFamily: 'Roboto, Archivo, Arial, sans-serif',
+    h1: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    h2: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    h3: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    h4: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    h5: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    h6: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    body1: {
+      fontFamily: 'Archivo, Arial, sans-serif',
+    },
+    body2: {
+      fontFamily: 'Archivo, Arial, sans-serif',
+    },
+    subtitle1: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    subtitle2: {
+      fontFamily: 'Roboto, Arial, sans-serif',
+    },
+    button: {
+      fontFamily: 'Archivo, Arial, sans-serif',
+    },
+    caption: {
+      fontFamily: 'Archivo, Arial, sans-serif',
+    },
+    overline: {
+      fontFamily: 'Archivo, Arial, sans-serif',
+    },
+  },
 });
 
 let lightTheme = createTheme({
@@ -47,6 +89,5 @@ let darkTheme = createTheme({
 
 lightTheme = responsiveFontSizes(lightTheme);
 darkTheme = responsiveFontSizes(darkTheme);
-console.log(darkTheme);
 
 export { lightTheme, darkTheme };
