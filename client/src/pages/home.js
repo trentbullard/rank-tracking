d23e746c1af2229d8f3eba28e45b86e68ad54295
trent bullard <trent.bullard@gmail.com>
2022-08-31 14:42:19 -0500

    start dashboard

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,21 @@
 import { Box, Typography } from '@mui/material';
 
 import { TitlePageLayout } from '../components/layouts';
+import { icons } from '../img/icons';
 
 const Home = () => {
+  const imgSize = "5rem";
   const currentUser = 'trent';
 
   return (
-    <TitlePageLayout title={`Hello ${currentUser}`} />
+    <TitlePageLayout title={`${currentUser}'s Dashboard`}>
+      <Box display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap" sx={{ width: "100%" }}>
+        <Box component="img" src={icons().foosball} alt="foosball" sx={{ height: imgSize, width: imgSize }} />
+        <Box component="img" src={icons().cornhole} alt="cornhole" sx={{ height: imgSize, width: imgSize }} />
+        <Box component="img" src={icons().tabletennis} alt="tabletennis" sx={{ height: imgSize, width: imgSize }} />
+        <Box component="img" src={icons().soccer} alt="soccer" sx={{ height: imgSize, width: imgSize }} />
+      </Box>
+    </TitlePageLayout>
   );
 };
 
