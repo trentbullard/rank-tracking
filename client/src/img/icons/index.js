d1b3d997622c7a56a5fe2c389625a4e28fbbb651
trent bullard <trent.bullard@gmail.com>
2022-08-31 14:41:14 -0500

    add soccer icon

--- client/src/img/icons/index.js
+++ client/src/img/icons/index.js
@@ -1,10 +1,13 @@
 import foosball from "./foosball.png";
 import cornhole from "./cornhole.png";
 import tabletennis from "./tabletennis.png";
+import soccer from "./soccer.png";
+
 export function icons() {
   return {
     foosball,
     cornhole,
     tabletennis,
+    soccer,
   };
 }
