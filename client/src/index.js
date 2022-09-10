ad87fbae0b012deedaddf54d5b80a50f16e5595d
trent bullard <trent.bullard@gmail.com>
2022-09-10 11:19:54 -0500

    cleanup auth routes

--- client/src/index.js
+++ client/src/index.js
@@ -18,12 +18,12 @@ import LeagueDetails from './pages/league/LeagueDetails';
 import NewLeague from './pages/league/NewLeague';
 import NotFound from './components/utility/404';
 
-const AuthRoute = ({ children, noAuth }) => {
+const AuthComponent = ({ element, noAuth }) => {
   const { currentUser } = React.useContext(AuthContext);
   const redirectTo = !!noAuth ? "/" : "/login";
-  return (!!noAuth !== !!currentUser) ? children : <Navigate to={redirectTo} replace />;
+  return (!!noAuth !== !!currentUser) ? element : <Navigate to={redirectTo} replace />;
 };
-
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -34,11 +34,11 @@ root.render(
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<App />}>
-                <Route index element={<AuthRoute><Home /></AuthRoute>} />
-                <Route path="leagues" element={<AuthRoute><Leagues /></AuthRoute>} />
-                <Route path="leagues/new" element={<AuthRoute><NewLeague /></AuthRoute>} />
-                <Route path="leagues/:id" element={<AuthRoute><LeagueDetails /></AuthRoute>} />
-                <Route path="login" element={<AuthRoute noAuth><Login /></AuthRoute>} />
+                <Route index element={<AuthComponent element={<Home />} />} />
+                <Route path="leagues" element={<AuthComponent element={<Leagues />} />} />
+                <Route path="leagues/new" element={<AuthComponent element={<NewLeague />} />} />
+                <Route path="leagues/:id" element={<AuthComponent element={<LeagueDetails />} />} />
+                <Route path="login" element={<AuthComponent element={<Login />} noAuth />} />
                 <Route path="*" element={<NotFound />} />
               </Route>
             </Routes>
