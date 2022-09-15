080d1ddcdd2b5582e60539c3df089da93a2a6c55
trent bullard <trent.bullard@gmail.com>
2022-09-15 09:27:36 -0500

    add headers to log deconstructed req params

--- middleware/log.js
+++ middleware/log.js
@@ -1,4 +1,4 @@
-export default ({ method, url, params, query, body }, res, next) => {
+export default ({ method, url, params, query, body, headers }, res, next) => {
   console.log(`\n[${new Date().toISOString()}]`);
   console.log(`Request: ${method} - ${url}`);
   console.log(JSON.stringify({ params, query, body, headers }, null, 2));
