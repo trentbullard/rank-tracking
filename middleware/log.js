8c2618f108624eabb4b39ef9de6a1998363274cd
trent bullard <trent.bullard@gmail.com>
2022-09-15 09:20:38 -0500

    add headers to api logging

--- middleware/log.js
+++ middleware/log.js
@@ -1,6 +1,6 @@
 export default ({ method, url, params, query, body }, res, next) => {
   console.log(`\n[${new Date().toISOString()}]`);
   console.log(`Request: ${method} - ${url}`);
-  console.log(JSON.stringify({ params, query, body }, null, 2));
+  console.log(JSON.stringify({ params, query, body, headers }, null, 2));
   next();
 };
