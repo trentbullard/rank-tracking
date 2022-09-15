48cbb1731a99b003de5a76a52da51db405406011
trent bullard <trent.bullard@gmail.com>
2022-09-15 10:11:22 -0500

    add filesize params to log4js, cleanup output

--- middleware/log.js
+++ middleware/log.js
@@ -2,7 +2,13 @@ import log4js from 'log4js';
 log4js.configure({
   appenders: {
     console: {type: 'console'},
-    file: {type: 'file', filename: 'logs/server.log'},
+    file: {
+      type: 'file',
+      filename: 'logs/server.log',
+      maxLogSize: 2621440,
+      backups: 3,
+      compress: true,
+    },
   },
   categories: {
     default: {appenders: ['console', 'file'], level: 'debug'},
@@ -11,8 +17,6 @@ log4js.configure({
 const logger = log4js.getLogger('server');
 
 export default ({ method, url, params, query, body }, res, next) => {
-  logger.info(`\n[${new Date().toISOString()}]`);
-  logger.info(`Request: ${method} - ${url}`);
-  logger.info(JSON.stringify({ params, query, body }, null, 2));
+  logger.info(`Request: ${method} - ${url}\n${JSON.stringify({ params, query, body }, null, 2)}`);
   next();
 };
