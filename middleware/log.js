4f6ca847eaa543ea844b52edeb75737fe7b542ae
trent bullard <trent.bullard@gmail.com>
2022-09-15 10:03:43 -0500

    implement log4js

--- middleware/log.js
+++ middleware/log.js
@@ -1,6 +1,18 @@
+import log4js from 'log4js';
+log4js.configure({
+  appenders: {
+    console: {type: 'console'},
+    file: {type: 'file', filename: 'logs/server.log'},
+  },
+  categories: {
+    default: {appenders: ['console', 'file'], level: 'debug'},
+  },
+});
+const logger = log4js.getLogger('server');
+
 export default ({ method, url, params, query, body }, res, next) => {
-  console.log(`\n[${new Date().toISOString()}]`);
-  console.log(`Request: ${method} - ${url}`);
-  console.log(JSON.stringify({ params, query, body }, null, 2));
+  logger.info(`\n[${new Date().toISOString()}]`);
+  logger.info(`Request: ${method} - ${url}`);
+  logger.info(JSON.stringify({ params, query, body }, null, 2));
   next();
 };
