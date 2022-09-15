875e97da9733489406f460b56c15ed5a8fc90df5
trent bullard <trent.bullard@gmail.com>
2022-09-15 09:50:32 -0500

    bump

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,10 @@ export default ({ method, _parsedUrl: { pathname }, headers: { authorization='' 
   const [type, token] = authorization.split(' ');
   const [thisMinute, lastMinute] = getThisAndLastMinute();
   const thisMinuteHash = digest(thisMinute + method + pathname);
-  console.log("🚀 ~ file: auth.js ~ line 8 ~ thisMinuteHash", thisMinuteHash)
   const lastMinuteHash = digest(lastMinute + method + pathname);
-  console.log("🚀 ~ file: auth.js ~ line 10 ~ lastMinuteHash", lastMinuteHash)
   if (type === 'Bearer' && (token === thisMinuteHash || token === lastMinuteHash)) {
     next();
   } else {
-    return res.status(401).json({ error: 'unauthorized' });
+    return res.status(401).json({ error: 'unauthorized: bad token' });
   };
 };
--- middleware/log.js
+++ middleware/log.js
@@ -1,6 +1,6 @@
-export default ({ method, url, params, query, body, headers: { authorization } }, res, next) => {
+export default ({ method, url, params, query, body }, res, next) => {
   console.log(`\n[${new Date().toISOString()}]`);
   console.log(`Request: ${method} - ${url}`);
-  console.log(JSON.stringify({ params, query, body, authorization }, null, 2));
+  console.log(JSON.stringify({ params, query, body }, null, 2));
   next();
 };
