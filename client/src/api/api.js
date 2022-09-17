c602e722b4b3179ac4c9ccaa5ccb1f006805d974
trent bullard <trent.bullard@gmail.com>
2022-09-17 13:44:06 -0500

    fix default api url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_MRANK_API_HOST || 'https://localhost:3002/api';
+const baseURL = process.env.REACT_APP_MRANK_API_HOST || 'http://localhost:3002/api';
 
 export default axios.create({ baseURL });
