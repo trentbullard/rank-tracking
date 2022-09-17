3d4d97db0a4c1e7ffb67c546e6e5bfc5db5ff133
trent bullard <trent.bullard@gmail.com>
2022-09-17 17:39:36 -0500

    implement league and season selection on new game form

--- client/src/components/game/NewGameForm.js
+++ client/src/components/game/NewGameForm.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import * as React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
 import {
   Box,
   IconButton,
   FormControl,
+  FormHelperText,
   InputLabel,
   MenuItem,
   Select,
@@ -12,8 +13,11 @@ import {
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { motion } from 'framer-motion';
 
+import api from '../../api/api';
 import { AuthContext } from '../../contexts/AuthContext';
-import { timedDigest, encrypt } from '../../helpers/cryptography';
+import { FlashContext } from '../../contexts/FlashContext';
+import { timedDigest } from '../../helpers/cryptography';
+import { isFalse, isTrue } from '../../helpers/boolean';
 
 let easing = [0.6, -0.05, 0.01, 0.99];
 const animate = {
@@ -26,27 +30,108 @@ const animate = {
   },
 };
 
+const LeagueSelector = ({ leagues, setLeague, selectedLeague, onClickAddLeague }) => {
+  return (
+    <Box display="flex" alignItems="flex-start" gap={1}>
+      <FormControl
+        fullWidth
+        disabled={isFalse(leagues)}
+      >
+        <InputLabel id="league-select-label">League</InputLabel>
+        <Select
+          labelId="league-select-label"
+          id="league-select"
+          value={selectedLeague}
+          label="League"
+          onChange={e => setLeague(e.target.value)}
+        >
+          {_.map(leagues, league => (
+            <MenuItem key={league.id} value={league.id}>
+              {league.name}
+            </MenuItem>
+          ))}
+        </Select>
+        {isFalse(leagues) && <FormHelperText component="span">No Leagues Found</FormHelperText>}
+      </FormControl>
+      <Tooltip title="Add League">
+        <IconButton onClick={onClickAddLeague} aria-label="add" color="secondary">
+          <AddCircleIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
+
+const SeasonSelector = ({ seasons, setSeason, selectedSeason, onClickAddSeason }) => {
+  return (
+    <Box display="flex" alignItems="flex-start" gap={1}>
+      <FormControl
+        fullWidth
+        disabled={isFalse(seasons)}
+      >
+        <InputLabel id="season-select-label">Season</InputLabel>
+        <Select
+          labelId="season-select-label"
+          id="season-select"
+          value={selectedSeason}
+          label="Season"
+          onChange={e => setSeason(e.target.value)}
+        >
+          {_.map(seasons, season => (
+            <MenuItem key={season.id} value={season.id}>{season.name}</MenuItem>
+          ))}
+        </Select>
+        {isFalse(seasons) && <FormHelperText>No Seasons Found</FormHelperText>}
+      </FormControl>
+      <Tooltip title="add season">
+        <IconButton onClick={onClickAddSeason} aria-label="add" color="secondary">
+          <AddCircleIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
+
 const NewGameForm = ({ sport }) => {
-  const [league, setLeague] = React.useState('');
-  const { currentUser } = React.useContext(AuthContext);
   const [leagues, setLeagues] = React.useState([]);
-  const data = encrypt(JSON.stringify({
-    currentUser,
-    sport,
-  }));
+  const [league, setLeague] = React.useState('');
+  const [seasons, setSeasons] = React.useState([]);
+  const [season, setSeason] = React.useState('');
+  const { currentUser } = React.useContext(AuthContext);
+  const { addFlash } = React.useContext(FlashContext);
   
   React.useEffect(() => {
+    if (isFalse(currentUser)) return;
     api.get(`/leagues`, {
       headers: {
         'Authorization': `Bearer ${timedDigest(`GET/api/leagues`)}`,
       },
-      params: {data},
+      params: {
+        owner_id: currentUser.id,
+        sport_id: sport.id,
+      },
     }).then(res => {
-      console.log(res.data);
-    }).catch(err => {
-      console.log(err);
+      setLeagues(res.data);
+    }).catch(error => {
+      addFlash(_.get(error, 'response.data.error', 'something went wrong'), 'error');
     });
-  }, []);
+  }, [currentUser, sport, addFlash]);
+
+  React.useEffect(() => {
+    if (isFalse(league)) return;
+    api.get(`/seasons`, {
+      headers: {
+        'Authorization': `Bearer ${timedDigest(`GET/api/seasons`)}`,
+      },
+      params: {
+        league_id: league,
+      },
+    }).then(res => {
+      setSeasons(res.data);
+    }).catch(error => {
+      addFlash(_.get(error, 'response.data.error', 'something went wrong'), 'error');
+    });
+  }, [league, addFlash]);
   
   const onSubmit = e => {
     e.preventDefault();
@@ -55,7 +140,12 @@ const NewGameForm = ({ sport }) => {
 
   const onClickAddLeague = e => {
     e.preventDefault();
-    console.log('add league');
+    addFlash('not implemented yet', 'info');
+  };
+  
+  const onClickAddSeason = e => {
+    e.preventDefault();
+    addFlash('not implemented yet', 'info');
   };
   
   return (
@@ -71,27 +161,8 @@ const NewGameForm = ({ sport }) => {
           initial={{ opacity: 0, y: 40 }}
           animate={animate}
         >
-          <Box display="flex" gap={1}>
-            <FormControl fullWidth>
-              <InputLabel id="league-select-label">League</InputLabel>
-              <Select
-                labelId="league-select-label"
-                id="league-select"
-                value={league}
-                label="League"
-                onChange={e => setLeague(e.target.value)}
-              >
-                {_.map(leagues, league => (
-                  <MenuItem key={league.id} value={league.id}>{league.name}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Tooltip title="add league" placement="top">
-              <IconButton onClick={onClickAddLeague} aria-label="add" component="span" color="secondary">
-                <AddCircleIcon fontSize="large" />
-              </IconButton>
-            </Tooltip>
-          </Box>
+          <LeagueSelector leagues={leagues} setLeague={setLeague} selectedLeague={league} onClickAddLeague={onClickAddLeague} />
+          {isTrue(league) && <SeasonSelector seasons={seasons} setSeason={setSeason} selectedSeason={season} onClickAddSeason={onClickAddSeason} />}
         </Box>
       </form>
     </Box>
--- client/src/pages/game/NewGame.js
+++ client/src/pages/game/NewGame.js
@@ -1,17 +1,44 @@
+import _ from 'lodash';
 import * as React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import api from '../../api/api';
+import { FlashContext } from '../../contexts/FlashContext';
 import FormLayout from '../../components/layouts/formPage/FormLayout';
 import NewGameForm from '../../components/game/NewGameForm';
+import { timedDigest } from '../../helpers/cryptography';
+import { isFalse } from '../../helpers/boolean';
 
 const NewGame = () => {
   const [searchParams] = useSearchParams();
-  const sport = searchParams.get('sport');
+  const [sport, setSport] = React.useState(null);
+  const { addFlash } = React.useContext(FlashContext);
+  const sportName = searchParams.get('sport');
+
+  React.useEffect(() => {
+    if (isFalse(sportName)) return;
+    api.get(`/sports`, {
+      headers: {
+        'Authorization': `Bearer ${timedDigest(`GET/api/sports`)}`,
+      },
+      params: {
+        name: sportName,
+      },
+    }).then(res => {
+      setSport(res.data[0]);
+    }).catch(error => {
+      addFlash(_.get(error, 'response.data.error', 'something went wrong'), 'error');
+    });
+  }, [sportName, addFlash]);
 
   return (
-    <FormLayout title={`new ${sport} game`}>
-      <NewGameForm sport={sport} />
-    </FormLayout>
+    isFalse(sport) ? (
+      <FormLayout title={`couldn't find ${sportName}`} />
+    ) : (
+      <FormLayout title={`new ${sport.name} game`}>
+        <NewGameForm sport={sport} />
+      </FormLayout>
+    )
   );
 };
 
