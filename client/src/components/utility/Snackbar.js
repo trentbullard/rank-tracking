8038ace26ae5095a3b37b2300aa1ce9b6374f290
trent bullard <trent.bullard@gmail.com>
2022-09-19 10:22:21 -0500

    reduce snackbar ttl

--- client/src/components/utility/Snackbar.js
+++ client/src/components/utility/Snackbar.js
@@ -24,7 +24,7 @@ const Snackbar = () => {
   return (
     <Stack spacing={2} sx={{ width: '100%' }}>
       {flashList.map((flash, index) => (
-        <MuiSnackbar open={open} autoHideDuration={6000} onClose={(e, reason) => dismiss(index, reason)} key={`snackbar-${index}`}>
+        <MuiSnackbar open={open} autoHideDuration={3000} onClose={(e, reason) => dismiss(index, reason)} key={`snackbar-${index}`}>
           <Alert onClose={(e, reason) => dismiss(index, reason)} severity={flash.type} sx={{ width: '100%' }}>
             {flash.message}
           </Alert>
