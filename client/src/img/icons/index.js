f5961ad1d6bef9e6f72306384ca661c217efccb3
Trent Bullard <trent.bullard@gmail.com>
2026-02-21 01:00:15 -0600

    billiards darts volleyball icons

--- client/src/img/icons/index.js
+++ client/src/img/icons/index.js
@@ -2,6 +2,9 @@ import foosball from "./foosball.png";
 import cornhole from "./cornhole.png";
 import tabletennis from "./tabletennis.png";
 import soccer from "./soccer.png";
+import volleyball from "./volleyball.png";
+import billiards from "./billiards.png";
+import darts from "./darts.png";
 
 export function icons() {
   return {
@@ -9,5 +12,8 @@ export function icons() {
     cornhole,
     tabletennis,
     soccer,
+    volleyball,
+    billiards,
+    darts,
   };
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,6 +10,9 @@ import { FlashContext } from '../contexts/FlashContext';
 import { TitlePageLayout } from '../components/layouts';
 import { icons } from '../img/icons';
 
+const NEW_GAME_BUTTON_SIZE = 88;
+const NEW_GAME_ICON_SIZE = 56;
+
 const StyledLink = styled(Link)({
   color: 'text.primary',
   textDecoration: 'none',
@@ -26,14 +29,27 @@ const StyledNewGameList = styled(Paper)({
 });
 
 const NewGameButton = ({sport}) => {
-  const imgSize = "100%";
   return (
-    <StyledLink component={RouterLink} to={`/games/new?sport=${sport}`}>
-      <Box display="flex" alignItems="center" flexDirection="column" sx={{ width: imgSize, height: imgSize }}>
-        <Box component="img" src={icons()[sport]} alt={`${sport}`} sx={{ height: imgSize, width: imgSize }} />
+    <StyledLink
+      component={RouterLink}
+      to={`/games/new?sport=${sport}`}
+      sx={{ width: `${NEW_GAME_BUTTON_SIZE}px` }}
+    >
+      <Box
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        sx={{ width: '100%', gap: 0.5 }}
+      >
+        <Box
+          component="img"
+          src={icons()[sport]}
+          alt={`${sport}`}
+          sx={{ width: `${NEW_GAME_ICON_SIZE}px`, height: `${NEW_GAME_ICON_SIZE}px`, objectFit: 'contain' }}
+        />
         <Typography
           variant="subtitle2"
-          sx={{ textTransform: "uppercase", fontWeight: "light", fontSize: "xx-small" }}
+          sx={{ textTransform: 'uppercase', fontWeight: 'light', fontSize: 'xx-small', textAlign: 'center' }}
         >
           {sport}
         </Typography>
@@ -70,7 +86,12 @@ const Home = () => {
           <StyledLink component={RouterLink} to="/games">GAMES</StyledLink>
         </Stack>
         <StyledNewGameList>
-          <Box display="flex" justifyContent="space-between" alignItems="flex-end" sx={{ width: "100%", gap: "1rem" }}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="flex-end"
+            sx={{ width: '100%', gap: '1rem', flexWrap: 'wrap' }}
+          >
             {_.map(sports, sport => (
               <NewGameButton sport={sport.name} key={sport.name} />
             ))}
